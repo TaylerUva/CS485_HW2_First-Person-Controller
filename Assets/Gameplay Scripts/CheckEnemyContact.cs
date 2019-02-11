fe5655336ecdc51d6b888cf13d106953d1268663
Tayler Uva <tayleruva@gmail.com>
2019-02-10 23:42:02 -0800

    fixed some mechanics

--- Assets/Gameplay Scripts/CheckEnemyContact.cs
+++ Assets/Gameplay Scripts/CheckEnemyContact.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CheckEnemyContact : MonoBehaviour {
-	private void OnTriggerEnter(Collider other) {
+	private void OnCollisionEnter(Collision other) {
 		if (other.gameObject.CompareTag("Enemy")) {
 			GetComponent<LoadScene>().LoadByIndex(2);
 		}
--- Assets/Gameplay Scripts/TrackPlayer.cs
+++ Assets/Gameplay Scripts/TrackPlayer.cs
@@ -7,24 +7,13 @@ public class TrackPlayer : MonoBehaviour {
 	public Transform target;
 	public float speed;
 	public float trackDistance;
-	private float targetY;
-
-	private void Start() {
-		targetY = target.position.y;
-	}
 	// Update is called once per frame
 	void Update() {
-		Vector3 targetPos = new Vector3(target.position.x, targetY, target.position.z);
+		Vector3 targetPos = new Vector3(target.position.x, 1, target.position.z);
 		transform.LookAt(targetPos, Vector3.up);
 		Vector3 trackedPos = transform.forward * speed * Time.deltaTime;
 		if (Vector3.Distance(targetPos, transform.position) < trackDistance) {
 			transform.position += new Vector3(trackedPos.x, 0, trackedPos.z);
-		}
-	}
-
-	private void OnTriggerEnter(Collider other) {
-		if (other.gameObject.CompareTag("Hammer") && Input.GetButton("Fire1")) {
-			Destroy(gameObject);
 		}
 	}
 }
