0752c56f87b49b6d2768b8943bbf697d693dde29
Tayler Uva <tayleruva@gmail.com>
2019-02-11 23:28:38 -0800

    created webbuild

--- Assets/Menu Scripts/OpenLink.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class OpenLink : MonoBehaviour {
-	public void OpenWebsite(string website) {
-		Application.OpenURL(website);
-	}
-}
