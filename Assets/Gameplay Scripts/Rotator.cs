a0e959c0bd8d4caa7f27f72992130c92d9ab1e27
Tayler Uva <tayleruva@gmail.com>
2019-02-11 22:08:45 -0800

    created map

---
+++ Assets/Gameplay Scripts/Rotator.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+	public Vector3 rotation = new Vector3(15, 30, 45);
+
+	void Update() {
+		transform.Rotate(rotation * Time.deltaTime);
+	}
+}
---
+++ Assets/Menu Scripts/WarpNextLevel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class WarpNextLevel : MonoBehaviour {
+
+	public int nextSceneIndex;
+
+	private void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag("Player")) {
+			if (nextSceneIndex >= 1) {
+				SceneManager.LoadScene(nextSceneIndex);
+			}
+		}
+	}
+}
