c7acf3e5c1eb70bf76631ff73145ed34abf2f241
Tayler Uva <tayleruva@gmail.com>
2019-02-11 22:32:16 -0800

    added level 2

--- Assets/Gameplay Scripts/CheckEnemyContact.cs
+++ Assets/Gameplay Scripts/CheckEnemyContact.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CheckEnemyContact : MonoBehaviour {
+	public int level;
+
 	private void OnCollisionEnter(Collision other) {
 		if (other.gameObject.CompareTag("Enemy")) {
-			GetComponent<LoadScene>().LoadByIndex(2);
+			GetComponent<LoadScene>().LoadByIndex(level);
 		}
 	}
 }
